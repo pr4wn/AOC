4c65756e009067eb00bfe1a3b43b42d04dab100f
Seán Doherty <sean.prawn@gmail.com>
2024-12-01 14:11:11 +0000

    Safer file opening

--- 2024/01/01-part1.pl
+++ 2024/01/01-part1.pl
@@ -4,12 +4,11 @@ use warnings;
 
 my $fn = "../data/01.txt";
 
-#my @a;
 my @col1;
 my @col2;
 my $gap = 0;
 
-open( my $fh, '<', $fn );
+open( my $fh, '<', $fn ) or die "Cannot open file $fn $!";
 
 while ( my $s = readline($fh) ) {
 
@@ -23,7 +22,6 @@ while ( my $s = readline($fh) ) {
 close $fh;
 
 @col1 = sort { $a <=> $b } @col1;
-
 @col2 = sort { $a <=> $b } @col2;
 
 for ( my $i = 0 ; $i <= $#col1 ; $i++ ) {
