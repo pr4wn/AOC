97736a3a6a0619667b800a09f5b72cd30877aef3
Seán Doherty <sean.prawn@gmail.com>
2024-12-01 13:33:05 +0000

    perl tidy and file rename

--- 2024/01/01-part1.pl
+++ 2024/01/01-part1.pl
@@ -4,30 +4,33 @@ use warnings;
 
 my $fn = "../data/01.txt";
 
+#my @a;
 my @col1;
 my @col2;
-my $sumsim = 0;
+my $gap = 0;
 
 open( my $fh, '<', $fn );
 
 while ( my $s = readline($fh) ) {
 
     chomp($s);
-    my @a = ( split '\s+', $s );
-    push @col1, $a[0];
-    push @col2, $a[1];
+    my @aa = ( split '\s+', $s );
+    push @col1, $aa[0];
+    push @col2, $aa[1];
 
 }
 
 close $fh;
 
+@col1 = sort { $a <=> $b } @col1;
+
+@col2 = sort { $a <=> $b } @col2;
+
 for ( my $i = 0 ; $i <= $#col1 ; $i++ ) {
 
-    my $j   = $col1[$i];
-    my $sim = grep { $j == $_ } @col2;
-    $sumsim += $j * $sim;
+    $gap += abs( $col1[$i] - $col2[$i] );
 
 }
 
-print "$sumsim\n";
+print "$gap\n";
 
